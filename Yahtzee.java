e1ecd2307f4ec4d2bcab816d982b4519c7b8065f
Samuel Moldenhauer <moldenhauers8522@my.uwstout.edu>
2024-09-16 16:04:22 -0500

    Initial Commit: Yahtzee class and Main function

---
+++ Yahtzee.java
@@ -0,0 +1,14 @@
+/* 
+Class: CS-358 Software Engineering Principles
+Proffessor: Jocelyn Richardt
+Assignment: Programming Assesment 1: Yahtzee
+*/
+
+//  Yahtzee class
+public class Yahtzee {
+
+    // Main Function
+    public static void main(String[] args) {
+        
+    }
+}
