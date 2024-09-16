5e22b6c6a3e4018cc59cdb7710b27f2a051f4aa0
Samuel Moldenhauer <moldenhauers8522@my.uwstout.edu>
2024-09-16 16:34:28 -0500

    Simple function to get the number of players.

--- Yahtzee.java
+++ Yahtzee.java
@@ -4,11 +4,28 @@ Proffessor: Jocelyn Richardt
 Assignment: Programming Assesment 1: Yahtzee
 */
 
-//  Yahtzee class
+// Class imports
+import java.util.Scanner;
+
+// Yahtzee class
 public class Yahtzee {
 
+    // This being a function is not currently needed given how simple it is. This allows for the simple ability to gather more user info in the future.
+    public static int GetPlayerInfo() {
+        int numOfPlayers =1;
+        String confirmation = "n";
+        Scanner userInput = new Scanner(System.in);
+
+        System.out.println("How many people will be playing? (1-6)");
+        numOfPlayers = userInput.nextInt();  
+
+        userInput.close();
+        return numOfPlayers;
+    }
     // Main Function
     public static void main(String[] args) {
-        
+        int players = GetPlayerInfo();
+
+        System.out.println(players + " people are playing Yahtzee!");
     }
 }
